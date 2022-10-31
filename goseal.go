7a2643fe89bdd365e8e18d6897734ba1e4497835
Max Breida <max.breida@fino.digital>
2022-10-31 09:48:24 +0100

    adds filemode selection, finishes first mvp of goseal tui

--- goseal.go
+++ goseal.go
@@ -1,17 +1,12 @@
 package main
 
 import (
-	"bytes"
 	"errors"
 	"fmt"
 	"log"
 	"os"
-	"os/exec"
-	"strings"
 
-	"github.com/MaxBreida/goseal/tui"
 	"github.com/urfave/cli/v2"
-	"gopkg.in/yaml.v2"
 )
 
 func main() {
@@ -23,7 +18,7 @@ func main() {
 			{
 				Name:        "ui",
 				Description: "starts the goseal TUI",
-				Action:      tui.StartUI,
+				Action:      StartUI,
 			},
 			{
 				Name:        "yaml",
@@ -97,26 +92,24 @@ func Yaml(c *cli.Context) error {
 	secretName := c.String("secret-name")
 	certPath := c.String("cert")
 
-	file, err := os.ReadFile(filePath)
+	secrets, err := GetSecretsFromFile("yaml", filePath, "")
 	if err != nil {
 		return err
 	}
 
-	if len(file) == 0 {
-		return ErrEmptyFile
+	var result []byte
+	if certPath != "" {
+		result, err = SealSecret(secrets, secretName, namespace, certPath)
+	} else {
+		result, err = CreateSecret(secrets, secretName, namespace)
 	}
-
-	var secrets map[string]string
-
-	if err := yaml.Unmarshal(file, &secrets); err != nil {
+	if err != nil {
 		return err
 	}
 
-	if certPath != "" {
-		return sealSecret(secrets, secretName, namespace, certPath)
-	}
+	fmt.Println(string(result))
 
-	return createSecret(secrets, secretName, namespace)
+	return nil
 }
 
 // File is a cli command
@@ -127,120 +120,22 @@ func File(c *cli.Context) error {
 	secretName := c.String("secret-name")
 	certPath := c.String("cert")
 
-	file, err := os.ReadFile(filePath)
+	secrets, err := GetSecretsFromFile("yaml", filePath, secretKey)
 	if err != nil {
 		return err
 	}
 
-	if len(file) == 0 {
-		return ErrEmptyFile
-	}
-
-	secrets := map[string]string{secretKey: string(file)}
-
+	var result []byte
 	if certPath != "" {
-		return sealSecret(secrets, secretName, namespace, certPath)
+		result, err = SealSecret(secrets, secretName, namespace, certPath)
+	} else {
+		result, err = CreateSecret(secrets, secretName, namespace)
 	}
-
-	return createSecret(secrets, secretName, namespace)
-}
-
-// runs the kubectl create secret command and prints the output to stdout.
-func createSecret(secrets map[string]string, secretName, namespace string) error {
-	kubectlCreateSecret := getCreateSecretFileCmd(secrets, secretName, namespace)
-
-	var stdout bytes.Buffer
-	kubectlCreateSecret.Stdout = &stdout
-
-	if err := runCommand(kubectlCreateSecret); err != nil {
-		return err
-	}
-
-	fmt.Println(stdout.String())
-
-	return nil
-}
-
-// runs the kubectl create secret command, pipes the output to the kubeseal command and prints the output to stdout.
-func sealSecret(secrets map[string]string, secretName, namespace, certPath string) error {
-	kubectlCreateSecret := getCreateSecretFileCmd(secrets, secretName, namespace)
-	kubeseal := exec.Command("kubeseal", "--format", "yaml", "--cert", certPath)
-
-	var (
-		err            error
-		stdout, stderr bytes.Buffer
-	)
-
-	kubeseal.Stdout = &stdout
-	kubeseal.Stderr = &stderr
-
-	// Get stdout of first command and attach it to stdin of second command.
-	kubeseal.Stdin, err = kubectlCreateSecret.StdoutPipe()
 	if err != nil {
 		return err
 	}
 
-	if err := kubeseal.Start(); err != nil {
-		return err
-	}
-
-	if err = runCommand(kubectlCreateSecret); err != nil {
-		return err
-	}
-
-	if err = kubeseal.Wait(); err != nil {
-		return errors.New(getErrText(err, kubeseal.Args, stderr.String()))
-	}
-
-	fmt.Println(stdout.String())
+	fmt.Println(string(result))
 
 	return nil
-}
-
-// retrieve a printable error text from cmd errors
-func getErrText(err error, cmdArgs []string, stdErr string) string {
-	text := fmt.Sprintf(
-		"command '%s' failed: %s",
-		strings.Join(cmdArgs, " "),
-		err.Error(),
-	)
-
-	errText := strings.TrimSpace(stdErr)
-	if len(errText) > 0 {
-		text += "\n" + errText
-	}
-
-	return text
-}
-
-func runCommand(cmd *exec.Cmd) error {
-	var stderr bytes.Buffer
-	cmd.Stderr = &stderr
-
-	if err := cmd.Run(); err != nil {
-		return errors.New(getErrText(err, cmd.Args, stderr.String()))
-	}
-
-	return nil
-}
-
-// creates
-func getCreateSecretFileCmd(secrets map[string]string, secretName, namespace string) *exec.Cmd {
-	args := []string{
-		"create",
-		"secret",
-		"generic",
-		secretName,
-		"-n",
-		namespace,
-		"--dry-run",
-		"-o",
-		"yaml",
-	}
-
-	for k, v := range secrets {
-		args = append(args, fmt.Sprintf("--from-literal=%s=%s", k, v))
-	}
-
-	return exec.Command("kubectl", args...)
 }
